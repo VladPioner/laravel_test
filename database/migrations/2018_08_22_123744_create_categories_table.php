37c7ccbc837d29b5782b0456396a370cac88cf48
Pioner <slavik8686@gmail.com>
2018-08-22 15:41:59 +0200

    create category admin

---
+++ database/migrations/2018_08_22_123744_create_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->integer('parent_id')->nullabel();
+            $table->tinyInteger('published')->nullabel();
+            $table->integer('created_by')->nullabel();
+            $table->integer('modified_by')->nullabel();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
